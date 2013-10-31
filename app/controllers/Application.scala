d29531595a2b0a16f50f80fda498dce24c751f16
Girish Gopaul <akhile@dr.com>
2013-10-31 17:11:46 +0000

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,11 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Forms._
+
+val taskForm = Form(
+  "label" -> nonEmptyText
+)
 
 object Application extends Controller {
 
