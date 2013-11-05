64631ecdd59d2efedd41dd83efaf5ffbb56f11bd
Girish Gopaul <akhile@dr.com>
2013-11-05 18:54:55 +0000

    Update 2.sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,7 +4,7 @@
 
 CREATE SEQUENCE user_id_seq;
 CREATE TABLE user (
-    id integer NOT NULL DEFAULT nextval('task_id_seq'),
+    id integer NOT NULL DEFAULT nextval('user_id_seq'),
     name varchar(255),
     email varchar(255)
 );
